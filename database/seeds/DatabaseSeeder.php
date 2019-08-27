893b642d1f108593ab23f58e353e47a5587b06e4
Amy <ampyaephyonaing@gmail.com>
2019-08-27 15:42:46 +0630

    Database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $admins = [
+            [
+                'display_name' => 'Amy Pyae Phyo Naing',
+                'phone' => '0943011736',
+                'password' => bcrypt('password')
+                
+            ],
+            [
+                'display_name' => 'May Thazin Oo',
+                'phone' => '092012345',
+                'password' => bcrypt('password')
+                
+            ],
+            [
+                'display_name' => 'Huai Siam',
+                'phone' => '092012346',
+                'password' => bcrypt('password')
+                
+            ]
+        ];
+
+        DB::table('users')->insert($admins);
     }
 }
